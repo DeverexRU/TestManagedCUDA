cbf94c71caf7ffd9f846c61bf8cd0ab5a8cbc90c
Deverex <deverex@yandex.ru>
2020-03-18 01:36:00 +0300

    Структура проекта

---
+++ Model/CUDARunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManagedCUDA.Model
+{
+    class CUDARunner
+    {
+
+        // 1.Простой тест 
+        // 2.Тест с передачей и приемом больших и разных структур
+
+    }
+}
